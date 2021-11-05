7bc494928e6233e123e88512b4bbc00d973c2d43
Raul-Soares <93782540+Raul-Soares@users.noreply.github.com>
2021-11-05 15:35:55 -0300

    Add files via upload

---
+++ ContaCorrenteFinal olimpiada udf.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+// Aqui serão codificadas as funçoes para verificar saldo, deposito e saque
+void ConsultaSaldo(float saldo){
+	printf("O saldo de sua conta conrrente e R$ %.2f.\n\n",saldo);
+	
+}
+
+float depositar(float sald_atual){
+float valorDep,novoSaldo;
+printf("informe o valor que deseja depositar \n");
+scanf("%f",&valorDep);
+if (valorDep>0){
+	printf("Deposito realizado com sucesso!\n\n");
+    novoSaldo = sald_atual + valorDep;
+}else{
+	printf("Valor invalido para deposito!\n\n");
+	novoSaldo=sald_atual;
+}
+return novoSaldo;
+}
+
+
+float sacar(float sald_atual){
+float valorSaque,novoSaldo;
+
+printf("informe o valor que deseja sacar \n");
+scanf("%f",&valorSaque);
+novoSaldo = sald_atual - valorSaque;
+if (novoSaldo>=0){
+	printf("Saque realizado com sucesso!\n\n");
+
+}else{
+	printf("Saldo insuficiente para o valor de saque!\n");
+	printf("Verifique o saldo disponivel\n\n");
+	novoSaldo=sald_atual;
+
+}
+return novoSaldo;
+
+}
+// Função principal com o Menu de opções
+int main(void) {
+	
+	int op, op2;
+	float saldo;
+	saldo = 00;
+	op2=1;
+	while(op2==1){
+	printf("Informe o numero da opcao desejada:\n");
+	printf("1-Saldo da conta corrente: \n");
+	printf("2-Deposito: \n");
+	printf("3-Saque: \n");
+	printf("0-para sair\n");
+    scanf("%d",&op);
+	
+  switch(op)
+{
+    case 1:
+            ConsultaSaldo(saldo);
+            
+            break;
+    
+    case 2:
+           saldo=depositar(saldo);
+            
+            break;
+    
+    case 3:
+            saldo=sacar(saldo);
+            break;
+    
+    default:
+    	if (op!=0){
+    		printf("Opcao invalida \n\n");
+		}
+            
+            }
+  if(op==0){
+    op2=0;
+    printf("Obrigado por utilizar nossos servicos!!");
+  }
+  
+}
+  }
+ 
